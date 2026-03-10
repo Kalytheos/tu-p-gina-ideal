1c9a66227bde042f65b76e0d23b7da3a25161892
kalytheos <dorianjosephes@gmail.com>
2026-03-10 05:07:10 -0500

    Configurar despliegue de GitHub Pages a carpeta docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/tu-p-gina-ideal/",
+  build: {
+    outDir: "docs",
+  },
   server: {
     host: "::",
     port: 8080,
